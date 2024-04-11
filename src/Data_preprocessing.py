5e0931826c0810ead16cb86b7c93a5be8c420e70
Abir Jamaly <abirjamaly2@gmail.com>
2024-04-11 19:36:05 -0400

    add balance_data function

--- src/Data_preprocessing.py
+++ src/Data_preprocessing.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.decomposition import PCA
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import LabelEncoder
+
 
 class Data_preprocessing: 
    def __init__(self, path): 
@@ -144,6 +146,18 @@ class Data_preprocessing:
       X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size = 0.2, random_state=42)
 
       return X_train, X_test, y_train, y_test
+
+   def balance_data(X_train, y_train):
+      """
+      Balances the classes in the dataset using SMOTE (Synthetic Minority Over-sampling Technique).
+
+      Returns:
+      - X_balanced: The feature matrix after balancing the classes.
+      - y_balanced: The target vector after balancing the classes.
+      """
+      smote = SMOTE()
+      X_balanced, y_balanced = smote.fit_resample(X_train, y_train)
+      return X_balanced, y_balanced
 
 
 
